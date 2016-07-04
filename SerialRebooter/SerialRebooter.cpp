2028f9197511794d6099b5ae48ac96963a740f9d
bknery <bknery@gmail.com>
2016-07-04 14:07:29 -0300

    SerialRebooter with Relay first working version

--- SerialRebooter/SerialRebooter.cpp
+++ SerialRebooter/SerialRebooter.cpp
@@ -17,13 +17,37 @@ SerialRebooter::SerialRebooter(std::string init_serial_device,
 	serial_device_ = init_serial_device;
 	total_reboots_ = init_total_reboots;
 	wait_ = init_wait;
+	reboot_device_ = SERIAL_CMD;
 	reboot_cmd_ = init_reboot_cmd;
 	find_pattern_ = init_find_pattern;
+	uri_device_ = "/dev/ttyACM0"; // dummy device
+	uri_port_ = 6;
+	off_time_ms_ = 1000;
 	result_ = 0;
 
-	std::cout << "Constructing serial rebooter object..." << std::endl;
+	std::cout << "Serial rebooter with commands through serial port..." << std::endl;
 }
 
+SerialRebooter::SerialRebooter(std::string init_serial_device,
+								int init_total_reboots,
+								int init_wait,
+								std::string init_uri_device,
+								int init_uri_port,
+								int init_off_time_ms,
+								std::string init_find_pattern) {
+	serial_device_ = init_serial_device;
+	total_reboots_ = init_total_reboots;
+	wait_ = init_wait;
+	reboot_device_ = URI_EL;
+	reboot_cmd_ = "reboot"; // dummy cmd
+	find_pattern_ = init_find_pattern;
+	uri_device_ = init_uri_device;
+	uri_port_ = init_uri_port;
+	off_time_ms_ = init_off_time_ms;
+	result_ = 0;
+
+	std::cout << "Serial rebooter with URI-EL..." << std::endl;
+}
 SerialRebooter::~SerialRebooter() {
 	std::cout << "Closing serial rebooter..." << std::endl;
 }
@@ -55,25 +79,13 @@ int SerialRebooter::Start(void){
 	// repeat for the total_reboots number of interactios
 	for (int i = 0; i < total_reboots_ ; i++) {
 
-		// open serial port for writing
-		std::cout << "Opening Serial Device for writing...";
-		std::ofstream serial_wr (serial_device_);
-
-		// check if serial was correctly open
-		if (serial_wr.is_open()) {
-			std::cout << "OK" << std::endl;
-		} else {
-			std::cout << std::endl << "ERROR opening serial port for writing." << std::endl;
+		std::cout << "ITERACTION: " << i << std::endl;
+		// reboot with serial command or with URI-EL
+		if (reboot() < 0) {
+			std::cout << "ERROR rebooting!" << std::endl;
 			return -1;
 		}
-		// send reboot command
-		serial_wr.write(reboot_cmd_.c_str(), reboot_cmd_.length());
-		serial_wr.write("\n", 1);
-		std::cout << "ITERACTION: " << i << std::endl;
-		std::cout << "SERIAL CMD: " << reboot_cmd_ << std::endl;
-
-		// close serial for writing
-		serial_wr.close();
+	
 
 		// read serial
 		// open serial port for reading
@@ -121,10 +133,109 @@ int SerialRebooter::GetResult(void){
 }
 
 void SerialRebooter::print_config(void){
-	
-	std::cout << "Serial sevice: " << serial_device_ << std::endl;
-	std::cout << "Total reboots: " << total_reboots_ << std::endl;
-	std::cout << "Wait: " << wait_ << std::endl;
-	std::cout << "Reboot cmd: " << reboot_cmd_ << std::endl;
-	std::cout << "Pattern to find: " << find_pattern_ << std::endl;
+	if (reboot_device_ == SERIAL_CMD) {
+		std::cout << "Serial sevice: " << serial_device_ << std::endl;
+		std::cout << "Total reboots: " << total_reboots_ << std::endl;
+		std::cout << "Wait: " << wait_ << std::endl;
+		std::cout << "Reboot device: Serial command" << std::endl;
+		std::cout << "Reboot cmd: " << reboot_cmd_ << std::endl;
+		std::cout << "Pattern to find: " << find_pattern_ << std::endl;
+	} else if (reboot_device_ == URI_EL) {
+		std::cout << "Serial sevice: " << serial_device_ << std::endl;
+		std::cout << "Total reboots: " << total_reboots_ << std::endl;
+		std::cout << "Wait: " << wait_ << std::endl;
+		std::cout << "Reboot device: URI-EL" << std::endl;
+		std::cout << "URI-EL device: " << uri_device_ << std::endl;
+		std::cout << "URI-EL port: " << uri_port_ << std::endl;
+		std::cout << "OFF time ms: " << off_time_ms_ << std::endl;
+		std::cout << "Pattern to find: " << find_pattern_ << std::endl;
+	}
+}
+
+int SerialRebooter::reboot(void) {
+	if (reboot_device_ == SERIAL_CMD) {
+		// open serial port for writing
+		std::cout << "Opening Serial Device for writing...";
+		std::ofstream serial_wr (serial_device_);
+
+		// check if serial was correctly open
+		if (serial_wr.is_open()) {
+			std::cout << "OK" << std::endl;
+		} else {
+			std::cout << std::endl << "ERROR opening serial port for writing." << std::endl;
+			return -1;
+		}
+		// send reboot command
+		serial_wr.write(reboot_cmd_.c_str(), reboot_cmd_.length());
+		serial_wr.write("\n", 1);
+		std::cout << "SERIAL CMD: " << reboot_cmd_ << std::endl;
+
+		// close serial for writing
+		serial_wr.close();
+
+	} else if (reboot_device_ == URI_EL) {
+		std::cout << "Turning URI-EL off..." << std::endl;
+		if (turn_uriel_off() < 0) {
+			std::cout << "Error turning URIEL off." << std::endl;
+			return -1;
+		}
+		// wait off time
+		std::cout << "Wait " << off_time_ms_ << " ms." << std::endl;
+		std::this_thread::sleep_for (std::chrono::milliseconds(off_time_ms_));
+
+		std::cout << "Turning URI-EL on..." << std::endl;
+		// turn on again
+		if (turn_uriel_on() < 0) {
+			std::cout << "Error turning URIEL on." << std::endl;
+			return -1;
+		}
+
+	}
+	return 0;
+}
+
+int SerialRebooter::turn_uriel_on(void) {
+
+	// open URIEL port for writing
+	std::cout << "Opening URIEL Device...";
+	std::ofstream uriel (uri_device_);
+
+	// check if uriel was correctly open
+	if (uriel.is_open()) {
+		std::cout << "OK" << std::endl;
+	} else {
+		std::cout << std::endl << "ERROR opening URIEL device." << std::endl;
+		return -1;
+	}
+	// turn URIEL on
+	for (unsigned int i = 0; i < sizeof(uri_on_cmd_); i++) {
+		uriel.put(uri_on_cmd_[i]);
+	}
+
+	// close URIEL device
+	uriel.close();
+	return 0;
+}
+
+int SerialRebooter::turn_uriel_off(void) {
+
+	// open URIEL port for writing
+	std::cout << "Opening URIEL Device...";
+	std::ofstream uriel (uri_device_);
+
+	// check if uriel was correctly open
+	if (uriel.is_open()) {
+		std::cout << "OK" << std::endl;
+	} else {
+		std::cout << std::endl << "ERROR opening URIEL device." << std::endl;
+		return -1;
+	}
+	// turn URIEL off
+	for (unsigned int i = 0; i < sizeof(uri_off_cmd_); i++) {
+		uriel.put(uri_off_cmd_[i]);
+	}
+
+	// close URIEL device
+	uriel.close();
+	return 0;
 }
--- SerialRebooter/SerialRebooterCmd.cpp
+++ SerialRebooter/SerialRebooterCmd.cpp
@@ -19,20 +19,46 @@ int main(int argc, char* argv[]){
 			std::cout << argv[i] << " ";
 		std::cout << std::endl;
 
-		// create object
-		SerialRebooter rebooter(std::string(argv[1]), 
+		if (std::string(argv[4]) == "cmd") {
+			//reboot with a serial command
+			// create object
+			// example command:
+			// SerialRebooterCmd /dev/ttyS0 10 4 cmd reset "dmboot: Board reset count"
+			SerialRebooter rebooter(std::string(argv[1]), 
 								std::stoi(argv[2]), 
 								std::stoi(argv[3]), 
-								std::string(argv[4]),
-								std::string(argv[5]));
+								std::string(argv[5]),
+								std::string(argv[6]));
 
-		// start the rebooter
-		rebooter.Start();
+			// start the rebooter
+			rebooter.Start();
+		} else if (std::string(argv[4]) == "uri") {
+			// reboot with URI-EL
+			// create object
+			// example command:
+			// SerialRebooterCmd /dev/ttyS0 10 4 uri /dev/ttyACM0 6 1000 "dmboot: Board reset count"
+			SerialRebooter rebooter(std::string(argv[1]), 
+								std::stoi(argv[2]), 
+								std::stoi(argv[3]), 
+								std::string(argv[5]),
+								std::stoi(argv[6]), 
+								std::stoi(argv[7]), 
+								std::string(argv[8]));
+
+			// start the rebooter
+			rebooter.Start();
+
+		} else {
+			print_help();
+		}
+
 	}
 	return 0;
 }
 
 void print_help(void){
-	std::cout << "Usage: SerialRebooterCmd <serial_device> <num_reboots> <wait_time_s> <reboot_cmd> <prompt>" << std::endl;
-	std::cout << "Example: SerialRebooterCmd /dev/ttyS0 100 30 reboot u-boot$" << std::endl;
+	std::cout << "Usage: SerialRebooterCmd <serial_device> <num_reboots> <wait_time_s> cmd <reboot_cmd> <find_pattern>" << std::endl;
+	std::cout << "Usage: SerialRebooterCmd <serial_device> <num_reboots> <wait_time_s> uri <uri_device> <uri_port> <uri_off_time_ms> <find_pattern>" << std::endl;
+	std::cout << "Example: SerialRebooterCmd /dev/ttyS0 100 4 cmd reboot u-boot$" << std::endl;
+	std::cout << "Example: SerialRebooterCmd /dev/ttyS0 100 4 uri /dev/ttyACM0 6 1000 u-boot$" << std::endl;
 }
