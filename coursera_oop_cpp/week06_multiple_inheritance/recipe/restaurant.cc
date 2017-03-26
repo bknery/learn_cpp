5b7ec6ed17f31bde63c2e015014b1d1b83fc39a5
bknery <bknery@gmail.com>
2017-03-26 11:54:18 -0300

    Coursera recipe exercise first compilation
    Signed-off-by: bknery <bknery@gmail.com>

--- coursera_oop_cpp/week06_multiple_inheritance/recipe/restaurant.cc
+++ coursera_oop_cpp/week06_multiple_inheritance/recipe/restaurant.cc
@@ -15,6 +15,8 @@ public:
   virtual string getNom() const {return nom_;}
   virtual string getUnite() const {return unite_;}
   virtual string toString() const {return nom_;}
+  virtual const Produit* adapter(double n) const;
+  virtual double quantiteTotale(const string& nomProduit) const;
   virtual ~Produit() {}
 
 protected:
@@ -22,25 +24,55 @@ protected:
   string unite_;
 };
 
+const Produit* Produit::adapter(double n) const {
+  return new Produit(*this);
+}
+
+double Produit::quantiteTotale(const string& nomProduit) const {
+  if (nom_ == nomProduit) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 class Ingredient {
 public:
   Ingredient(const Produit& produit, double quantite) :
     produit_(produit), quantite_(quantite) {}
   const Produit& getProduit() const {return produit_;}
   double getQuantite() const {return quantite_;}
+  void setQuantite(double quantite) {quantite_ = quantite;}
+  double quantiteTotale(const string& nomProduit) const;
+  string descriptionAdaptee() const;
 
 protected:
-  const Produit produit_;
+  const Produit& produit_;
   double quantite_;
 };
+
+string Ingredient::descriptionAdaptee() const {
+
+  const Produit* pproduit(produit_.adapter(quantite_));
+
+  ostringstream strs;
+  strs << to_string(quantite_) << " " << pproduit->getUnite() << " de " << pproduit->toString();
+  delete pproduit;
+  return strs.str();
+}
+
+double Ingredient::quantiteTotale(const string& nomProduit) const {
+  return quantite_ * produit_.quantiteTotale(nomProduit);
+}
 
 class Recette {
 public:
   Recette(string nom, double nbFois = 1.0) :
     nom_(nom), nbFois_(nbFois) {}
   void ajouter(const Produit& p, double quantite);
-  Recette adapter(double n);
-  string toString();
+  Recette adapter(double n) const;
+  string toString() const;
+  double quantiteTotale(const string& nomProduit) const;
   ~Recette();
 
 protected:
@@ -50,15 +82,85 @@ protected:
 };
 
 void Recette::ajouter(const Produit& p, double quantite) {
-  ingredients_.push_back(new Ingredient(p, quantite));
+  ingredients_.push_back(new Ingredient(p, nbFois_ * quantite));
 }
+
+Recette Recette::adapter(double n) const {
+  Recette recette(*this);
+  for (auto ingredient : recette.ingredients_) {
+    double quantite(ingredient->getQuantite());
+    ingredient->setQuantite(quantite * n);
+  }
+  recette.nbFois_ = n * nbFois_;
+  return recette;
+}
+
+string Recette::toString() const {
+  ostringstream strs;
+  strs << "Recette \"" << nom_ << "\" x " << nbFois_ << endl;
+
+  int i = 1;
+  for (vector<Ingredient*>::const_iterator it = ingredients_.begin(); it != ingredients_.end(); ++it) {
+    strs << i << ". " << (*it)->descriptionAdaptee();
+    if (it != (ingredients_.end() - 1)) {
+      strs << endl;
+    }
+    ++i;
+  }
+
+  return strs.str();
+}
+
+double Recette::quantiteTotale(const string& nomProduit) const {
+  double quantite(0);
+  for (auto ingredient : ingredients_) {
+    if (ingredient->getProduit().getNom() == nomProduit) {
+      quantite += ingredient->getQuantite();
+    }
+  }
+  return quantite;
+}
+
+Recette::~Recette() {}
 
 class ProduitCuisine : public Produit {
 public:
   ProduitCuisine(string nom) : Produit(nom, "portion(s)"), recette_(nom) {}
+  virtual void ajouterARecette(const Produit& produit, double quantite);
+  virtual const ProduitCuisine* adapter(double n) const override;
+  virtual string toString() const override;
+  virtual double quantiteTotale(const string& nomProduit) const override;
+  virtual ~ProduitCuisine() {}
 private:
   Recette recette_;
 };
+
+void ProduitCuisine::ajouterARecette(const Produit& produit, double quantite) {
+  recette_.ajouter(produit, quantite);
+}
+
+const ProduitCuisine* ProduitCuisine::adapter(double n) const {
+  ProduitCuisine* pproduit(new ProduitCuisine(*this));
+  pproduit->recette_.adapter(n);
+
+  return pproduit;
+}
+
+string ProduitCuisine::toString() const {
+  ostringstream strs;
+  strs << Produit::toString() << endl;
+  strs << recette_.toString();
+  return strs.str();
+}
+
+double ProduitCuisine::quantiteTotale(const string& nomProduit) const {
+  if (nom_ == nomProduit) {
+    return 1;
+  } else {
+    return recette_.quantiteTotale(nomProduit);
+  }
+}
+
 
 /*******************************************
  * Ne rien modifier après cette ligne.
