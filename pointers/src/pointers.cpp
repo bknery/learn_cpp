1037de9d0edfda591b96a1560b72771ddddd14f3
Bruno Nery <bknery@gmail.com>
2017-08-16 10:08:40 -0300

    Pointers test with printf
    Signed-off-by: Bruno Nery <bknery@gmail.com>

--- pointers/src/pointers.cpp
+++ pointers/src/pointers.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
+#include <stdio.h>
 
 void f(int* p);
 
 int main(){
 	int x = 47;
+
+    int y = 21;
+    int *ptr = &y;
+
 	std::cout << "x = " << x << std::endl;
 	std::cout << "&x = " << &x << std::endl;
 	f(&x);
 	std::cout << "x = " << x << std::endl;
+
+    printf("Pointers test:\n");
+    printf("y = %d\n", y);
+    printf("ptr = %p\n", ptr);
+    printf("&y = 0x%p\n", &y);
+    printf("*ptr = %d\n", *ptr);
+
 }
 
 void f(int* p){
